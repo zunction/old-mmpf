6b8d5bfb6b6fc4c48a7ef7e8d82e047fde88395e
Zhangsheng Lai <dunno.noe@gmail.com>
2017-06-01 12:37:15 +0800

    gradient checking and theory update

--- filtermpf/mpfutils.py
+++ filtermpf/mpfutils.py
@@ -11,10 +11,16 @@ from datetime import datetime
 
 def load_data(dataset = '32-50K.npy', borrow = True):
     """
-    Loads the dataset.
+    Loads the dataset. With Theano consideration.
     """
 
     data = np.load(dataset)
     dataset = theano.shared(np.asarray(data, dtype = theano.config.floatX), borrow = borrow)
 
     return dataset
+
+# def load_data(dataset = '32-50K.npy'):
+#     """
+#     Loads the dataset.
+#     """
+#     return np.load(dataset)
